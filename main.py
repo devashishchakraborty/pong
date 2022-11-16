566788f261794c832349dead8ff5487911087b48
Devashish Chakraborty <devashishchakra@gmail.com>
2022-11-16 23:59:26 +0530

    Added Animation

--- main.py
+++ main.py
@@ -11,12 +11,39 @@ screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong")
 
+# Game Rectangles
+ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
+player = pygame.Rect(screen_width -20, screen_height/2 - 70, 8, 140)
+opponent = pygame.Rect(10, screen_height/2 - 70, 8, 140)
+
+
+bg_color = pygame.Color("grey12")
+light_grey = (200, 200, 200)
+
+ball_speed_x = 7
+ball_speed_y = 7
+
 while True:
 	# Handling Input
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
+
+	ball.x += ball_speed_x
+	ball.y += ball_speed_y
+
+	if ball.right >= screen_width or ball.left <= 0:
+		ball_speed_x *= -1
+	if ball.bottom >= screen_height or ball.top <= 0:
+		ball_speed_y *= -1
+
+	# Visuals
+	screen.fill(bg_color)
+	pygame.draw.rect(screen, light_grey, player)
+	pygame.draw.rect(screen, light_grey, opponent)
+	pygame.draw.ellipse(screen, light_grey, ball)
+	pygame.draw.aaline(screen, light_grey, (screen_width/2,0), (screen_width/2, screen_height))
 
 	# Updating the window
 	pygame.display.flip()
